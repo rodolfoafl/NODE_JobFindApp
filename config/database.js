4a5add03d2bc767a7778d3488f2eedb8a4730153
Rodolfo Leal <rodolfoafl@gmail.com>
2019-07-17 22:42:59 -0300

    Implemented database.js

---
+++ config/database.js
@@ -0,0 +1,13 @@
+//Sequelize
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize('codegig', 'postgres', 'raf1992leal', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+  }
+});

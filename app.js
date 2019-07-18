251818e69c0a62ec9159e3e53883a93cce7ea8fc
Rodolfo Leal <rodolfoafl@gmail.com>
2019-07-17 22:42:42 -0300

    Implemented app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+//Database
+const db = require('./config/database');
+
+//Test DB
+db.authenticate()
+    .then(() => console.log('Database connected...'))
+    .catch(err => console.error(err));
+
+
+const app = express();
+
+app.get('/', (req, res) => res.send('INDEX'));
+
+//Gig routes
+app.use('/gigs', require('./routes/gigs'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server starter at port ${PORT}`));

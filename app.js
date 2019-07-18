02eda8f53b2f2b9473768d98d1b567e1678b0c56
Rodolfo Leal <rodolfoafl@gmail.com>
2019-07-18 16:25:16 -0300

    Updated app.js

--- app.js
+++ app.js
@@ -14,7 +14,20 @@ db.authenticate()
 
 const app = express();
 
-app.get('/', (req, res) => res.send('INDEX'));
+//Handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Index route
+app.get('/', (req, res) => res.render('index', {layout: 'landing'}));
 
 //Gig routes
 app.use('/gigs', require('./routes/gigs'));
